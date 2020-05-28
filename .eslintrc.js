31018d405d5fac00f7cabdad075666a0562bd127
Gabriele Morisco <gabrielemori@outlook.com>
2020-05-29 00:02:48 +0200

    eslint and prettier configured

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,6 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
   },
   rules: {
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_|^req|^next' }],
