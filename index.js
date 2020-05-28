5db494a012a43fb79b96878a821ecbcbff850789
Gabriele Morisco <gabrielemori@outlook.com>
2020-05-29 00:04:14 +0200

    code format

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 require('dotenv').config();
 
-const homedir = require('homedir')
+const homedir = require('homedir');
 const { env } = process;
 const deviceCredentials = require(env.DEVICECREDENTIALS_FILE);
 const GoogleAssistant = require('./src/services/GoogleAssistant/googleassistant');
 
 const CREDENTIALS = {
-    client_id: deviceCredentials.client_id,
-    client_secret: deviceCredentials.client_secret,
-    refresh_token: deviceCredentials.refresh_token,
-    type: "authorized_user"
+  client_id: deviceCredentials.client_id,
+  client_secret: deviceCredentials.client_secret,
+  refresh_token: deviceCredentials.refresh_token,
+  type: 'authorized_user',
 };
 
 const assistant = new GoogleAssistant(CREDENTIALS);
 const stdio = require('stdio');
 // Allow user to continually input questions and receive answers.
 const promptUser = () => {
-    stdio.question('> ', (err, prompt) => {
-        assistant.assist(prompt)
-            .then(({ text }) => {
-                console.log(text); // Will log the answer
-                promptUser();
-            });
+  stdio.question('> ', (err, prompt) => {
+    assistant.assist(prompt).then(({ text }) => {
+      console.log(text); // Will log the answer
+      promptUser();
     });
+  });
 };
 
 promptUser();
