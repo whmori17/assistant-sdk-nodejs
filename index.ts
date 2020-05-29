08de6985013ffd3e374c50856eec9af73365d67f
Gabriele Morisco <gabrielemori@outlook.com>
2020-05-29 23:27:07 +0200

    ternary boolean

--- index.ts
+++ index.ts
@@ -16,10 +16,8 @@ const assistantCredentials = {
 
 const assistant = new GoogleAssistant(assistantCredentials);
 
-if (typeof process.argv[2] !== 'undefined') {
-  assistant.assist(process.argv[2]).then(({ text }) => {
-    console.log(text);
-  });
-} else {
-  promptUser(assistant);
-}
+typeof process.argv[2] !== 'undefined'
+  ? assistant.assist(process.argv[2]).then(({ text }) => {
+      console.log(text);
+    })
+  : promptUser(assistant);
